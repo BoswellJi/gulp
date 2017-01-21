66bbae4228d15a7c1da1872c856507e01e124740
jmz <aiyoudqrjmz@163.com>
2017-01-21 14:16:51 +0800

    同步

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,17 @@
- let jquery = require('jquery');
+var map = new BMap.Map("app"); // 创建地图实例  
+var point = new BMap.Point(116.404, 39.915); // 创建点坐标  
+map.centerAndZoom(point, 15); // 初始化地图，设置中心点坐标和地图级别
 
- function ab() {
-     console.log(jquery);
- }
+// 给地图添加右键菜单
+var contextMenu = new BMap.ContextMenu();
+contextMenu.addItem(new BMap.MenuItem('<h2>text: String</h2>', function() { alert('右键') }, { width: '200px', id: 'one', iconUrl: BMAP_CONTEXT_MENU_ICON_ZOOMIN }));
+map.addContextMenu(contextMenu);
 
- ab();
+// 给地图添加覆盖物
+// var overlay = BMap.Overlay();
 
- let p = new Promise(function() {
+BMap.Overlay.show();
+map.addOverlay(new BMap.Marker((new BMap.Point(116.404, 39.915), {icon:}));
 
- }, function() {
-
- });
-
-
- console.log('fffdff');
+map.disableDragging()
+    // map.enableScrollWheelZoom()()
--- src/jsx/react.jsx
+++ src/jsx/react.jsx
@@ -1,14 +1,14 @@
-var React=require('react');
-var ReactDOM=require('react-dom');
+// var React=require('react');
+// var ReactDOM=require('react-dom');
 
-var App=React.createClass({
-    render:function(){
-            return (
-                <div>
-                      sd  appdfdsdfdfdfdlllf
-                </div>
-            );
-    }
-});
+// var App=React.createClass({
+//     render:function(){
+//             return (
+//                 <div>
+//                       sd  appdfdsdfdfdfdlllf
+//                 </div>
+//             );
+//     }
+// });
 
-ReactDOM.render(<App />,document.querySelector('#app'));
+// ReactDOM.render(<App />,document.querySelector('#app'));
